77a0676d2586a25d9beed7a2494233f8a3837df6
Igor Jesus Silva Leon <147112193+IgorjsLeon@users.noreply.github.com>
2023-10-19 22:41:54 -0300

    Update exercicio_Aula08.py

--- Aula08/exercicio_Aula08.py
+++ Aula08/exercicio_Aula08.py
@@ -8,51 +8,84 @@ def valor_Compras_Periodo(estoque_Inicial, estoque_Final):
     print("O valor compras:", valor_Compras_Periodo)
     """
 
-from funcoes_Clientes import menu_Cliente, ver_Cliente, inserir_Clienteveiculo, att_Cliente_Nome, menu_Att, att_Cliente_Telefone, att_Cliente_Email , att_Cliente_Marca, att_Cliente_Cor, att_Cliente_Placa
-lista_Cliente = []
-numeros_clientes_inseridos = set()
+
+from funcoes_Clientes import menu_Cliente, inserir_cliente_veiculo, ver_cliente, menu_Att, att_Cliente_Nome, att_Cliente_Telefone, att_Cliente_Email, att_Cliente_Marca, att_Cliente_Cor, att_Cliente_Placa
+
+lista_clientes = []
+cliente_existente = []
 while True:
     menu_Cliente()
-    opcao_Menu = int(input("Digite a opção desejada(1 ~ 5): "))
-
-    if opcao_Menu == 1:
-        num_Cliente = int(input("\nDigite o número do cliente(1 ~ ...): "))
-        if num_Cliente not in numeros_clientes_inseridos:
-            nome = input("Digite o nome do cliente: ")
+    opcao_menu = input("Digite a opção desejada: ")
+    if opcao_menu == '1':
+        num_cliente = int(input("Digite o número do cliente: "))
+        if num_cliente in cliente_existente:
+            print("Cliente já existe, utilize a opção 3 para atualiza-lo.")
+        else:
+            nome = input("Digite o nome completo do cliente: ")
             telefone = int(input("Digite o telefone do cliente: "))
             email = input("Digite o e-mail do cliente: ")
-            marca = input("Digite a marca do carro do cliente: ")
-            cor = input("Digite a cor do veiculo do cliente: ")
-            placa = input("Digite a placa do veiculo do cliente: ")
-            inserir_Clienteveiculo(num_Cliente, nome, telefone, email, marca, cor, placa)
+            marca = input("Digite a marca do veículo do cliente: ")
+            placa = input("Digite a marca do veículo do cliente: ")
+            cor = input("Digite a cor do veículo do cliente: ")
+            inserir_cliente_veiculo(num_cliente, nome, telefone, email, marca, placa, cor)
+            cliente_existente.append(num_cliente)
+            print("Cliente cadastrado com sucesso.")
+        
+    elif opcao_menu == '2':
+        ver_cliente()
 
-            # Adicione o número do cliente ao conjunto
-            numeros_clientes_inseridos.add(num_Cliente)
+    elif opcao_menu == '3':
+        menu_Att()
+        opcao_menu = input("Digite a opção desejada: ")
+        #------Nome------
+        if opcao_menu == '1':
+            num_cliente = int(input("Digite o número do cliente: "))
+            if num_cliente in cliente_existente:
+               novo_Nome = input("Digite o novo nome: ")
+               att_Cliente_Nome(num_cliente,novo_Nome)
+            else:
+                print("Cliente não encontrado.")
+        #------Telefone------
+        elif opcao_menu == '2':
+            num_cliente = int(input("Digite o número do cliente: "))
+            if num_cliente in cliente_existente:
+                att_Cliente_Telefone()
+                print("Telefone alterado com sucesso.")
+            else:
+                print("Cliente não encontrado.")
+        #------E-mail------
+        elif opcao_menu == '3':
+            num_cliente = int(input("Digite o número do cliente: "))
+            if num_cliente in cliente_existente:
+                att_Cliente_Email()
+                print("E-mail alterado com sucesso.")
+            else:
+                print("Cliente não encontrado.")
+        #------Marca------
+        elif opcao_menu == '4':
+            num_cliente = int(input("Digite o número do cliente: "))
+            if num_cliente in cliente_existente:
+                att_Cliente_Marca()
+                print("Marca alterado com sucesso.")
+            else:
+                print("Cliente não encontrado.")
+        #------Placa------
+        elif opcao_menu == '5':
+            num_cliente = int(input("Digite o número do cliente: "))
+            if num_cliente in cliente_existente:
+                att_Cliente_Placa()
+                print("Placa alterado com sucesso.")
+            else:
+                print("Cliente não encontrado.")
+        #------Cor------
+        elif opcao_menu == '6':
+            num_cliente = int(input("Digite o número do cliente: "))
+            if num_cliente in cliente_existente:
+                att_Cliente_Cor()
+                print("Cor alterado com sucesso.")
+            else:
+                print("Cliente não encontrado.")
         else:
-            print("Este cliente ja existe, caso queira atualizar use a função número 3.")
-    elif opcao_Menu == 2:
-        ver_Cliente()
-    elif opcao_Menu == 3:
-        num_Cliente = int(input("Digite o número do cliente: "))
-        cliente = None  # Inicialize como None
-        for c in lista_Cliente:
-            if c['Numero Cliente'] == num_Cliente:
-                cliente = True  # Encontrou o cliente
-                break
-        if cliente == True:
-            menu_Att()
-            opcao_Att = int(input("Digite a opção desejada: "))
-            if opcao_Att == 1:
-                att_Cliente_Nome(cliente)
-            elif opcao_Att == 2:
-                att_Cliente_Telefone(cliente)
-            elif opcao_Att == 3:
-                att_Cliente_Email(cliente)
-            elif opcao_Att == 4:
-                att_Cliente_Marca(cliente)
-            elif opcao_Att == 5:
-                att_Cliente_Cor(cliente)
-            elif opcao_Att == 6:
-                att_Cliente_Placa(cliente)
-        else:
-            print("Cliente não encontrado.")
+            print("Opção invalida.") 
+    else:
+        print("Opção invalida.") 
